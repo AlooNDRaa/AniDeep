5455f478306ea958991829f16528d26ca025fced
Alondra <alonngadea@gmail.com>
2023-10-16 15:47:11 -0300

    Refactor: read me and changes in styles

--- frontend/src/assets/components/header.jsx
+++ frontend/src/assets/components/header.jsx
@@ -1,5 +1,6 @@
 import {BsIncognito} from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import {TbPointerSearch} from 'react-icons/tb';
 import '../styles/App.css';
 
 
@@ -31,7 +32,7 @@ export function Header() {
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
+              <button className="btn btn-outline-success" type="submit"><TbPointerSearch/></button>
             </form>
           </div>
         </div>
