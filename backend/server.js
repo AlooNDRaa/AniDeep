a04ecde55b8a4d32b98685d0bca4d57a3c0fab4b
Alondra <alonngadea@gmail.com>
2023-12-14 12:30:09 -0300

    Refactor database model anideep

--- backend/server.js
+++ backend/server.js
@@ -24,15 +24,15 @@ animes.forEach(element => {
     view_url: element.view_url || ''
   });
 
-   Anime.create(anime, (err, data) => {
-     if (err) {
-       console.error("Error al crear el anime:", err);
-     } else {
-      console.log("Anime guardado con éxito:", data);
-     }
-   });
+//    Anime.create(anime, (err, data) => {
+//      if (err) {
+//        console.error("Error al crear el anime:", err);
+//      } else {
+//       console.log("Anime guardado con éxito:", data);
+//      }
+//    });
 
-  //Con esto se me repite el guardado de información (LLanto)
+//   //Con esto se me repite el guardado de información (LLanto)
 });
 
 app.get('/myapi/animeclass/date', (req, res) => {
